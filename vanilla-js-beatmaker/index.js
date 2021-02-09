72dc8440ef5827807f1a9845c38ea4d7b88ed9f3
Khanh <Khanh13Nguyen13@gmail.com>
2021-02-10 00:49:54 +0200

    Pause & Clear buttons

--- vanilla-js-beatmaker/index.js
+++ vanilla-js-beatmaker/index.js
@@ -1,72 +1,88 @@
-window.addEventListener('load', ()=> {
-  const sounds = document.querySelectorAll('.sound');
-  const pads = document.querySelectorAll('.pads div');
-  const visaul = document.querySelector('.visual');
-  const btnPlay = document.querySelector('#js-play');
-  const btnRecord = document.querySelector('#js-record');
-  const colors = [
-    "#2ecc71",
-    "#d36060",
-    "#c060d3",
-    "#d3d160",
-    "#606bd3",
-    "#60c2d3"
-  ];
-  let recordList = [];
-  
-  pads.forEach((pad, index) => {
-    pad.addEventListener('click', ()=> {
-      sounds[index].currentTime = 0;
-      sounds[index].play();
-      createBubble(index);
-    })
+	const sounds = document.querySelectorAll('.sound');
+	const pads = document.querySelectorAll('.pads div');
+	const visaul = document.querySelector('.visual');
+	const btnPlay = document.querySelector('#js-play');
+	const btnRecord = document.querySelector('#js-record');
+  const btnPause = document.querySelector('#js-pauseRecord');
+  const btnReset = document.querySelector('#js-reset');
+	const colors = ['#2ecc71', '#d36060', '#c060d3', '#d3d160', '#606bd3', '#60c2d3'];
+	let recordList = [];
 
-    const createBubble = (index)=> {
-      const bubble = document.createElement('div');
-      visaul.appendChild(bubble);
-      bubble.style.backgroundColor = colors[index];
-      bubble.style.animation = 'jump 1s ease';
-      bubble.addEventListener('animationend', function() {
-        visaul.removeChild(this)
-      });
-    }
-  });
+	pads.forEach((pad, index) => {
+		pad.addEventListener('click', () => {
+			sounds[index].currentTime = 0;
+			sounds[index].play();
+			createBubble(index);
+		});
+	});
 
-  //SET UP RECORD BUTTON
-  btnRecord.addEventListener('click', ()=> {
+  const createBubble = (index) => {
+    const bubble = document.createElement('div');
+    visaul.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = 'jump 1s ease';
+    bubble.addEventListener('animationend', function () {
+      visaul.removeChild(this);
+    });
+  };
+
+
+	//SET UP BUTTONS
+	btnRecord.addEventListener('click', recordBeat);
+  btnPlay.addEventListener('click', playBeat);
+  btnPause.addEventListener('click', pauseRecord);
+  btnReset.addEventListener('click', resetAll);
+
+
+  // SET UP FUNCTIONS
+	function playBeat() {
+    btnReset.disabled = false;
+    btnRecord.classList.add('is-active');
+    
+		for (let i = 0; i < recordList.length; i++) {
+			setTimeout(() => {
+				recordList[i].play();
+			}, 500 * i + 1);
+		}
+	}
+
+  function recordBeat() {
+    btnPause.disabled = false;
+		
+    this.classList.add('is-active');
+
+		pads.forEach((pad, index) => {
+			pad.addEventListener('click', () => {
+				sounds[index].currentTime = 0;
+				sounds[index].play();
+				recordList.push(sounds[index]);
+				createBubble(index);
+			});
+		});
+    // const createBubble = (index) => {
+    //   const bubble = document.createElement('div');
+    //   visaul.appendChild(bubble);
+    //   bubble.style.backgroundColor = colors[index];
+    //   bubble.style.animation = 'jump 1s ease';
+    //   bubble.addEventListener('animationend', function () {
+    //     visaul.removeChild(this);
+    //   });
+    // };
+  }
+
+  function pauseRecord() {
     btnPlay.disabled = false;
 
-    pads.forEach((pad, index) => {
-      pad.addEventListener('click', ()=> {
-        sounds[index].currentTime = 0;
-        sounds[index].play();
-        recordList.push(sounds[index]);
-        createBubble(index);
-        console.log(recordList);
-      })
-  
-      const createBubble = (index)=> {
-        const bubble = document.createElement('div');
-        visaul.appendChild(bubble);
-        bubble.style.backgroundColor = colors[index];
-        bubble.style.animation = 'jump 1s ease';
-        bubble.addEventListener('animationend', function() {
-          visaul.removeChild(this)
-        });
-      }
-    });
-  })
+    btnRecord.classList.remove('is-active');
+    btnRecord.removeEventListener('click', recordBeat)
+  }
 
-  //SET UP PLAY BUTTON
-  btnPlay.addEventListener('click', ()=> {
-    playList(recordList);
-  });
+  function resetAll() {
+    recordList = [];
+    btnPause.disabled = true;
+    btnPlay.disabled = true;
+    btnReset.disabled = true;
 
-  function playList(list) {
-    for (let i = 0; i < list.length; i++) {
-      setTimeout(()=> {
-        list[i].play();
-      }, 1000 * i);
-    }
-  };
-});
+    btnRecord.classList.remove('is-active');
+    btnRecord.addEventListener('click', recordBeat);
+  }
