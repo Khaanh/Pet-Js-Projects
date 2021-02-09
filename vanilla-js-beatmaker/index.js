343067acffbe2ac988e5da59834a730798ee6fd0
Khanh <Khanh13Nguyen13@gmail.com>
2021-02-09 19:09:14 +0200

    Start record & play buttons

--- vanilla-js-beatmaker/index.js
+++ vanilla-js-beatmaker/index.js
@@ -2,8 +2,8 @@ window.addEventListener('load', ()=> {
   const sounds = document.querySelectorAll('.sound');
   const pads = document.querySelectorAll('.pads div');
   const visaul = document.querySelector('.visual');
-  const playAgain = document.querySelector('.play');
-
+  const btnPlay = document.querySelector('#js-play');
+  const btnRecord = document.querySelector('#js-record');
   const colors = [
     "#2ecc71",
     "#d36060",
@@ -13,15 +13,12 @@ window.addEventListener('load', ()=> {
     "#60c2d3"
   ];
   let recordList = [];
-
+  
   pads.forEach((pad, index) => {
     pad.addEventListener('click', ()=> {
       sounds[index].currentTime = 0;
       sounds[index].play();
-      recordList.push(sounds[index]);
       createBubble(index);
-
-      console.log(recordList);
     })
 
     const createBubble = (index)=> {
@@ -35,7 +32,33 @@ window.addEventListener('load', ()=> {
     }
   });
 
-  playAgain.addEventListener('click', ()=> {
+  //SET UP RECORD BUTTON
+  btnRecord.addEventListener('click', ()=> {
+    btnPlay.disabled = false;
+
+    pads.forEach((pad, index) => {
+      pad.addEventListener('click', ()=> {
+        sounds[index].currentTime = 0;
+        sounds[index].play();
+        recordList.push(sounds[index]);
+        createBubble(index);
+        console.log(recordList);
+      })
+  
+      const createBubble = (index)=> {
+        const bubble = document.createElement('div');
+        visaul.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = 'jump 1s ease';
+        bubble.addEventListener('animationend', function() {
+          visaul.removeChild(this)
+        });
+      }
+    });
+  })
+
+  //SET UP PLAY BUTTON
+  btnPlay.addEventListener('click', ()=> {
     playList(recordList);
   });
 
